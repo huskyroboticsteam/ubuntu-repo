bce4abf29887902539bc002d59b420a3fb667d8d
Evan Straw <evan.straw99@gmail.com>
2022-04-18 22:29:10 -0700

    Update RPLidar to v1.10.1-3

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -9,7 +9,7 @@ CATCH2_URL="https://github.com/catchorg/catch2"
 UBLOX_URL="https://github.com/huskyroboticsteam/ublox-linux"
 
 if [[ -z $1 || "$1" == "urg-lidar" ]]; then "$BUILD" "$URG_LIDAR_URL" "v1.2.5-2"; fi
-if [[ -z $1 || "$1" == "rplidar" ]]; then "$BUILD" "$RPLIDAR_URL" "v1.10.0-2"; fi
+if [[ -z $1 || "$1" == "rplidar" ]]; then "$BUILD" "$RPLIDAR_URL" "v1.10.0-3"; fi
 if [[ -z $1 || "$1" == "catch2" ]]; then "$BUILD" "$CATCH2_URL" "v2.13.7"; fi
 if [[ -z $1 || "$1" == "ublox" ]]; then "$BUILD" "$UBLOX_URL" "v2.0.6-1"; fi
 
