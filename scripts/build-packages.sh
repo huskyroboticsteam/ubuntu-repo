fa314976505eb8a5edffe61f12b54dc564576984
Evan Straw <evan.straw99@gmail.com>
2022-04-23 12:31:03 -0700

    Add HindsightCAN library

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -7,11 +7,13 @@ URG_LIDAR_URL="https://github.com/huskyroboticsteam/urg-lidar"
 RPLIDAR_URL="https://github.com/huskyroboticsteam/rplidar"
 CATCH2_URL="https://github.com/catchorg/catch2"
 UBLOX_URL="https://github.com/huskyroboticsteam/ublox-linux"
+CAN_URL="https://github.com/huskyroboticsteam/HindsightCAN"
 
 if [[ -z $1 || "$1" == "urg-lidar" ]]; then "$BUILD" "$URG_LIDAR_URL" "v1.2.5-2"; fi
 if [[ -z $1 || "$1" == "rplidar" ]]; then "$BUILD" "$RPLIDAR_URL" "v1.10.0-3"; fi
 if [[ -z $1 || "$1" == "catch2" ]]; then "$BUILD" "$CATCH2_URL" "v2.13.7"; fi
 if [[ -z $1 || "$1" == "ublox" ]]; then "$BUILD" "$UBLOX_URL" "v2.0.6-1"; fi
+if [[ -z $1 || "$1" == "hindsightcan" ]]; then "$BUILD" "$CAN_URL" "v1.0.0"; fi
 
 touch -t 197001010000.00 *.deb
 
