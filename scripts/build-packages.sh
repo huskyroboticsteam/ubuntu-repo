5cad10d3b3deb383380635a80b9d3a80b6dc675b
Evan Straw <evan.straw99@gmail.com>
2022-04-23 12:46:44 -0700

    Update HindsightCAN to v1.0.1

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -13,7 +13,7 @@ if [[ -z $1 || "$1" == "urg-lidar" ]]; then "$BUILD" "$URG_LIDAR_URL" "v1.2.5-2"
 if [[ -z $1 || "$1" == "rplidar" ]]; then "$BUILD" "$RPLIDAR_URL" "v1.10.0-3"; fi
 if [[ -z $1 || "$1" == "catch2" ]]; then "$BUILD" "$CATCH2_URL" "v2.13.7"; fi
 if [[ -z $1 || "$1" == "ublox" ]]; then "$BUILD" "$UBLOX_URL" "v2.0.6-1"; fi
-if [[ -z $1 || "$1" == "hindsightcan" ]]; then "$BUILD" "$CAN_URL" "v1.0.0"; fi
+if [[ -z $1 || "$1" == "hindsightcan" ]]; then "$BUILD" "$CAN_URL" "v1.0.1"; fi
 
 touch -t 197001010000.00 *.deb
 
